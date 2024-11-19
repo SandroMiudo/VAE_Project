074813b90c40589cc22213004d4849c55e0c2b93
SandroMiudo <sandromiudo0@gmail.com>
2024-11-19 10:47:32 +0100

    added some types

--- src/utils/_c_types.py
+++ src/utils/_c_types.py
@@ -1,6 +1,10 @@
-from typing import Tuple
+from typing import Tuple, Mapping, Any
+from numpy import ndarray
 
 type Properties = Tuple[int, int]
 type Globals = Tuple[int, int, float, float]
 type Strategy = str
 type _size = int
+
+type NdArrayLike = ndarray
+type MapLike = Mapping[str, Any]
